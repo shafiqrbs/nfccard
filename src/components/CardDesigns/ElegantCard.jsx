50c2f64d03753779e5fab5d23992ceb8390a9573
foysal-mahmud-hasan <1730150@iub.edu.bd>
2024-06-02 16:47:30 +0600

    card added

--- src/components/CardDesigns/ElegantCard.jsx
+++ src/components/CardDesigns/ElegantCard.jsx
@@ -1,41 +1,61 @@
-import { Card, Avatar, Text, Group, Box, Image, Divider, BackgroundImage } from '@mantine/core';
+import { Card, Avatar, Text, Group, Box, Image, Divider, BackgroundImage, Grid } from '@mantine/core';
 import { IconPhone, IconMail, IconMapPin } from '@tabler/icons-react';
 import { readLocalStorageValue } from '@mantine/hooks';
 
 function ElegantCard() {
     const formData = readLocalStorageValue({ key: 'signup-form-data' });
     return (
-        <Card shadow="lg" padding="lg" radius="md" style={{ width: '85.6mm', height: '53.98mm' }}>
-            <BackgroundImage src={formData.companyLogo} radius="md" height={50}>
-                <Box sx={{ textAlign: 'center', padding: '1rem 0' }}>
-                    <Avatar
-                        src={formData.profilePic}
-                        size={80}
-                        radius="xl"
-                        sx={{ margin: 'auto', border: '2px solid white', position: 'relative', top: '20px' }}
-                    />
-                </Box>
-            </BackgroundImage>
-            <Box textAlign="center" mt={50}>
-                <Text weight={700} size="md">Foysal Mahmud Hasan</Text>
-                <Text c="dimmed" size="sm">Jr. Software Engineer</Text>
+        <Card shadow="lg" padding="xs" radius="md" style={{ width: '100mm', height: '60mm' }} bg={'var(--mantine-color-gray-0)'}>
+            <Box textAlign="center" mt={'xs'} ml={'xs'} mb={10}>
+                <Text weight={700} size="md">{formData.name}</Text>
+                <Text c="dimmed" size="sm">{formData.designation}</Text>
             </Box>
+            <div style={{ position: 'relative', width: '100%', textAlign: 'end' }}>
+                <Avatar
+                    src={formData.profilePic}
+                    size={70}
+                    radius="100%"
+                    style={{ position: 'absolute', top: -65, left: '90%', transform: 'translateX(-50%)', border: '1px solid white' }}
+                />
+            </div>
+
             <Divider my="sm" />
             <Box textAlign="left">
-                <Group spacing="xs">
-                    <IconPhone size={16} />
-                    <Text size="sm">+8801231234751</Text>
-                </Group>
-                <Group spacing="xs">
-                    <IconMail size={16} />
-                    <Text size="sm">foysalmahmud.rbs@gmail.com</Text>
-                </Group>
-                <Group spacing="xs">
-                    <IconMapPin size={16} />
-                    <Text size="sm">29, Gaussia Azam Avenue, Uttara, Dhaka</Text>
-                </Group>
+                <Grid columns={12} gutter={0}>
+                    <Grid.Col span={1}>
+                        <IconPhone size={16} />
+                    </Grid.Col>
+                    <Grid.Col span={11}>
+                        <Text size="sm">{formData.phone}</Text>
+                    </Grid.Col>
+                </Grid>
+                <Grid columns={12} gutter={0}>
+                    <Grid.Col span={1}>
+                        <IconMail size={16} />
+                    </Grid.Col>
+                    <Grid.Col span={11}>
+                        <Text size="sm">{formData.email}</Text>
+                    </Grid.Col>
+                </Grid>
+                <Grid columns={12} gutter={0}>
+                    <Grid.Col span={1}>
+                        <IconMapPin size={16} />
+                    </Grid.Col>
+                    <Grid.Col span={11}>
+                        <Text size="sm">{formData.address}{formData.address}</Text>
+                    </Grid.Col>
+                </Grid>
+
             </Box>
-        </Card>
+            <div style={{ position: 'relative', width: '100%', textAlign: 'end', marginTop: 5, marginBottom: 5 }} >
+                <img
+                    src={formData.companyLogo}
+                    alt="Company Logo"
+                    style={{ width: '100%', height: 30, objectFit: 'contain', borderRadius: 'md' }}
+                />
+            </div>
+
+        </Card >
     );
 }
 
